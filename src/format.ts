63e04e2c8f0d697cf1e88c1ccf60bc5d49b23562
Geoffrey Litt <gklitt@gmail.com>
2021-06-08 16:12:18 -0400

    apply prettier formatting

--- src/format.ts
+++ src/format.ts
@@ -1,12 +1,12 @@
 import type { ResolvedOp } from "./operations"
 import { MarkType } from "./schema"
 
-export type FormatSpan = { marks: MarkType[], start: number }
+export type FormatSpan = { marks: MarkType[]; start: number }
 
 /** Given a log of operations, produce the final flat list of format spans */
 export function replayOps(ops: ResolvedOp[]): FormatSpan[] {
-  const initialSpans: FormatSpan[] = [{ marks: [], start: 0 }]
-  return ops.reduce(applyOp, initialSpans)
+    const initialSpans: FormatSpan[] = [{ marks: [], start: 0 }]
+    return ops.reduce(applyOp, initialSpans)
 }
 
 /** Given a list of spans and a formatting op,
@@ -18,27 +18,27 @@ export function replayOps(ops: ResolvedOp[]): FormatSpan[] {
  *    which can be translated into PM transactions?
  */
 function applyOp(spans: FormatSpan[], op: ResolvedOp): FormatSpan[] {
-  const coveringStartIndex = spanCovering(spans, op.start)
-  const coveringEndIndex = spanCovering(spans, op.end)
+    const coveringStartIndex = spanCovering(spans, op.start)
+    const coveringEndIndex = spanCovering(spans, op.end)
 
-  // In this case, a single existing span covered the whole range of our op
-  if (coveringStartIndex === coveringEndIndex) {
-    const coveringSpan = spans[coveringStartIndex]
-    const newSpans: FormatSpan[] = [
-      { start: op.start, marks: applyFormatting(coveringSpan.marks, op) },
-      { start: op.end + 1, marks: coveringSpan.marks }
-    ]
+    // In this case, a single existing span covered the whole range of our op
+    if (coveringStartIndex === coveringEndIndex) {
+        const coveringSpan = spans[coveringStartIndex]
+        const newSpans: FormatSpan[] = [
+            { start: op.start, marks: applyFormatting(coveringSpan.marks, op) },
+            { start: op.end + 1, marks: coveringSpan.marks },
+        ]
 
-    return [
-      ...spans.slice(0, coveringStartIndex + 1),
-      ...newSpans,
-      ...spans.slice(coveringStartIndex + 1)
-    ]
-  } else {
-    throw new Error('unimplemented')
-  }
+        return [
+            ...spans.slice(0, coveringStartIndex + 1),
+            ...newSpans,
+            ...spans.slice(coveringStartIndex + 1),
+        ]
+    } else {
+        throw new Error("unimplemented")
+    }
 
-  return spans
+    return spans
 }
 
 /** Given a list of spans and a position in the document,
@@ -46,12 +46,12 @@ function applyOp(spans: FormatSpan[], op: ResolvedOp): FormatSpan[] {
  *  Currently a naive linear scan; could be made faster.
  */
 function spanCovering(spans: FormatSpan[], position: number): number {
-  const indexAfter = spans.findIndex(span => span.start > position)
-  if (indexAfter === -1) {
-    return spans.length - 1
-  } else {
-    return indexAfter - 1
-  }
+    const indexAfter = spans.findIndex(span => span.start > position)
+    if (indexAfter === -1) {
+        return spans.length - 1
+    } else {
+        return indexAfter - 1
+    }
 }
 
 /** Return a new list of marks after applying an op;
@@ -59,16 +59,16 @@ function spanCovering(spans: FormatSpan[], position: number): number {
  *  (does not mutate the passed-in list of marks)
  */
 function applyFormatting(marks: MarkType[], op: ResolvedOp): MarkType[] {
-  switch (op.type) {
-    case 'addMark': {
-      if (!marks.includes(op.markType)) {
-        return [...marks, op.markType]
-      } else {
-        return marks
-      }
+    switch (op.type) {
+        case "addMark": {
+            if (!marks.includes(op.markType)) {
+                return [...marks, op.markType]
+            } else {
+                return marks
+            }
+        }
+        case "removeMark": {
+            return marks.filter(mark => mark != op.markType)
+        }
     }
-    case 'removeMark': {
-      return marks.filter(mark => mark != op.markType)
-    }
-  }
 }
--- test/inlineFormatSpans.ts
+++ test/inlineFormatSpans.ts
@@ -1,95 +1,94 @@
 import assert from "assert"
-import { FormatSpan, replayOps } from '../src/format'
+import { FormatSpan, replayOps } from "../src/format"
 import { ResolvedOp } from "../src/operations"
 
 describe("applying format spans", function () {
-  describe("with no ops", function () {
-    const ops: ResolvedOp[] = []
-    it("returns a single span starting at 0", function () {
-      assert.deepStrictEqual(replayOps(ops), [{ marks: [], start: 0 }
-      ])
+    describe("with no ops", function () {
+        const ops: ResolvedOp[] = []
+        it("returns a single span starting at 0", function () {
+            assert.deepStrictEqual(replayOps(ops), [{ marks: [], start: 0 }])
+        })
     })
-  })
 
-  describe("with adding one bold span", function () {
-    // 01234567890123456789
-    //   |------| b
-    // _______________________
-    // |-|
-    //   |------| b
-    //          |----------|
-    const ops: ResolvedOp[] = [
-      { type: "addMark", markType: "strong", start: 2, end: 9 },
-    ]
+    describe("with adding one bold span", function () {
+        // 01234567890123456789
+        //   |------| b
+        // _______________________
+        // |-|
+        //   |------| b
+        //          |----------|
+        const ops: ResolvedOp[] = [
+            { type: "addMark", markType: "strong", start: 2, end: 9 },
+        ]
 
-    const expected: FormatSpan[] = [
-      { marks: [], start: 0 },
-      { marks: ["strong"], start: 2 },
-      { marks: [], start: 10 },
-    ]
+        const expected: FormatSpan[] = [
+            { marks: [], start: 0 },
+            { marks: ["strong"], start: 2 },
+            { marks: [], start: 10 },
+        ]
 
-    it("returns the expected result", function () {
-      assert.deepStrictEqual(replayOps(ops), expected)
+        it("returns the expected result", function () {
+            assert.deepStrictEqual(replayOps(ops), expected)
+        })
     })
-  })
 
-  describe("with bold, unbold, then bold, all overlapping", function () {
-    // 01234567890123456789
-    //   |------| b
-    //     |-------| !b
-    //           |----| b
-    // _______________________
-    // |-|
-    //   |--| b
-    //     |-----|
-    //           |----| b
-    //                 |--|
-    const ops: ResolvedOp[] = [
-      { type: "addMark", markType: "strong", start: 2, end: 9 },
-      { type: "removeMark", markType: "strong", start: 5, end: 13 },
-      { type: "addMark", markType: "strong", start: 11, end: 16 },
-    ]
+    describe("with bold, unbold, then bold, all overlapping", function () {
+        // 01234567890123456789
+        //   |------| b
+        //     |-------| !b
+        //           |----| b
+        // _______________________
+        // |-|
+        //   |--| b
+        //     |-----|
+        //           |----| b
+        //                 |--|
+        const ops: ResolvedOp[] = [
+            { type: "addMark", markType: "strong", start: 2, end: 9 },
+            { type: "removeMark", markType: "strong", start: 5, end: 13 },
+            { type: "addMark", markType: "strong", start: 11, end: 16 },
+        ]
 
-    const expected: FormatSpan[] = [
-      { marks: [], start: 0 },
-      { marks: ["strong"], start: 2 },
-      { marks: [], start: 5 },
-      { marks: ["strong"], start: 11 },
-      { marks: [], start: 16 }
-    ]
+        const expected: FormatSpan[] = [
+            { marks: [], start: 0 },
+            { marks: ["strong"], start: 2 },
+            { marks: [], start: 5 },
+            { marks: ["strong"], start: 11 },
+            { marks: [], start: 16 },
+        ]
 
-    it("returns the expected result", function () {
-      assert.deepStrictEqual(replayOps(ops), expected)
+        it("returns the expected result", function () {
+            assert.deepStrictEqual(replayOps(ops), expected)
+        })
     })
-  })
 
-  describe("with bold, unbold, then italic", function () {
-    // 01234567890123456789
-    //   |------| b
-    //     |-------| !b
-    //           |----| b
-    // _______________________
-    // |-|
-    //   |--| b
-    //     |-----|
-    //           |----| b
-    //                 |--|
+    describe("with bold, unbold, then italic", function () {
+        // 01234567890123456789
+        //   |------| b
+        //     |-------| !b
+        //           |----| b
+        // _______________________
+        // |-|
+        //   |--| b
+        //     |-----|
+        //           |----| b
+        //                 |--|
 
-    const ops: ResolvedOp[] = [
-      { type: "addMark", markType: "strong", start: 4, end: 14 },
-      { type: "removeMark", markType: "strong", start: 9, end: 19 },
-      { type: "addMark", markType: "em", start: 1, end: 19 },
-    ]
+        const ops: ResolvedOp[] = [
+            { type: "addMark", markType: "strong", start: 4, end: 14 },
+            { type: "removeMark", markType: "strong", start: 9, end: 19 },
+            { type: "addMark", markType: "em", start: 1, end: 19 },
+        ]
 
-    const expected: FormatSpan[] = [
-      { marks: [], start: 0 },
-      { marks: ["em"], start: 1 },
-      { marks: ["em", "strong"], start: 4 },
-      { marks: ["em"], start: 9 },
-    ]
+        const expected: FormatSpan[] = [
+            { marks: [], start: 0 },
+            { marks: ["em"], start: 1 },
+            { marks: ["em", "strong"], start: 4 },
+            { marks: ["em"], start: 9 },
+        ]
 
-    it("returns the expected result", function () {
-      assert.deepStrictEqual(replayOps(ops), expected)
+        it("returns the expected result", function () {
+            assert.deepStrictEqual(replayOps(ops), expected)
+        })
     })
-  })
 })

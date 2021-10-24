40050dcf41acbf31d2c76d8fb184d3b2cbe62bb5
Peter van Hardenberg <pvh@pvh.ca>
2021-10-24 13:35:33 -0700

    also remove marks

--- test/fuzz.ts
+++ test/fuzz.ts
@@ -3,18 +3,38 @@ import crypto from "crypto"
 import Micromerge, { Change } from "../src/micromerge"
 import { generateDocs } from "./generateDocs"
 
-function addMarkChange(doc: Micromerge) {
+type MarkTypes = "strong" | "em"
+const markTypes: MarkTypes[] = ["strong", "em"]
+
+function addConflictingMarkChange(doc: Micromerge) {
     const length = (doc.root.text as any[]).length
     const start = Math.floor(Math.random() * length)
     const end = start + Math.floor(Math.random() * (length - start - 1))
+    const markType = markTypes[Math.floor(Math.random() * markTypes.length)];
     const { change } = doc.change([
         {
             path: ["text"],
             action: "addMark",
             start,
             end,
-            markType: "comment",
-            attrs: { id: `abc-${Math.floor(Math.random() * 1000)}` },
+            markType
+        },
+    ])
+    return change
+}
+
+function removeMarkChange(doc: Micromerge) {
+    const length = (doc.root.text as any[]).length
+    const start = Math.floor(Math.random() * length)
+    const end = start + Math.floor(Math.random() * (length - start - 1))
+    const markType = markTypes[Math.floor(Math.random() * markTypes.length)];
+    const { change } = doc.change([
+        {
+            path: ["text"],
+            action: "removeMark",
+            start,
+            end,
+            markType
         },
     ])
     return change
@@ -58,13 +78,15 @@ function removeChange(doc: Micromerge) {
     return change
 }
 
-// eslint-disable-next-line no-constant-condition
 const [doc1, doc2] = generateDocs("alphabet")
 const doc1Queue: Change[] = []
 const doc2Queue: Change[] = []
-const opTypes = ["insert", "remove", "addMark"]
+const opTypes = ["insert", "remove", "addMark", "removeMark"]
 
-while(true) {
+// eslint-disable-next-line no-constant-condition
+let totalChanges = 0
+while(totalChanges++ < 1_000_000) {
+    if (totalChanges % 1000 == 0) { console.log("Total changes: ", totalChanges) }
     const randomTarget = (Math.random() < 0.5)
     const doc = randomTarget ? doc1 : doc2
     const queue = randomTarget ? doc1Queue : doc2Queue
@@ -72,30 +94,33 @@ while(true) {
     const op = opTypes[Math.floor(Math.random() * opTypes.length)];
     switch(op) {
         case "insert":
-            console.log(`Inserting into ${randomTarget ? "doc1" : "doc2"}`)
+            console.log(`I ${randomTarget ? "doc1" : "doc2"}`)
             queue.push(insertChange(doc))
             break
         case "remove":
-            console.log(`Deleting from ${randomTarget ? "doc1" : "doc2"}`)
+            console.log(`D ${randomTarget ? "doc1" : "doc2"}`)
             queue.push(removeChange(doc))
             break
         case "addMark":
-            console.log(`Adding mark into ${randomTarget ? "doc1" : "doc2"}`)
-            queue.push(addMarkChange(doc))
+            console.log(`K ${randomTarget ? "doc1" : "doc2"}`)
+            queue.push(addConflictingMarkChange(doc))
             break
-    }
+        case "removeMark":
+            console.log(`K ${randomTarget ? "doc1" : "doc2"}`)
+            queue.push(removeMarkChange(doc))
+            break
+        }
 
-    const shouldSync = (Math.random() < 0.1)
+    const shouldSync = (Math.random() < 0.02)
     if (shouldSync) {
-        console.log(`Merging ${doc1Queue.length} doc1 changes`)
-        console.log(`Merging ${doc2Queue.length} doc2 changes`)
+        console.log(`M doc1: ${doc1Queue.length} doc2: ${doc2Queue.length}`)
 
         doc1Queue.forEach(c => doc2.applyChange(c))
         doc2Queue.forEach(c => doc1.applyChange(c))
         doc1Queue.length = 0
         doc2Queue.length = 0 // typical JS "elegance"
 
-        console.log(doc1.root.text.join(''))
+        console.log(doc1.getTextWithFormatting(["text"]))
         assert.deepStrictEqual(
             doc1.getTextWithFormatting(["text"]),
             doc2.getTextWithFormatting(["text"]),

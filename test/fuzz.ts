05ba2c1001b1b1a9f9266d739382c0c83104377f
Peter van Hardenberg <pvh@pvh.ca>
2021-10-25 14:23:17 -0700

    move fuzz out of normal testing, fix handling of comments

--- test/fuzz.ts
+++ test/fuzz.ts
@@ -6,10 +6,12 @@ import { generateDocs } from "./generateDocs"
 type MarkTypes = "strong" | "em" | "link" | "comment"
 const markTypes: MarkTypes[] = ["strong", "em", "link", "comment"]
 
-const exampleURLs = ["https://inkandswitch.com", 
-                     "https://inkandswitch.com/cambria/",
-                     "https://inkandswitch.com/peritext/",
-                     "https://inkandswitch.com/pushpin"]
+const exampleURLs = ["https://inkandswitch.com",
+    "https://inkandswitch.com/cambria/",
+    "https://inkandswitch.com/peritext/",
+    "https://inkandswitch.com/pushpin"]
+
+const commentHistory: string[] = []
 
 function addConflictingMarkChange(doc: Micromerge) {
     const length = (doc.root.text as any[]).length
@@ -19,42 +21,41 @@ function addConflictingMarkChange(doc: Micromerge) {
     if (markType === "link") {
         const url = exampleURLs[Math.floor(Math.random() * exampleURLs.length)];
         const { change } = doc.change([
-        {
-            path: ["text"],
-            action: "addMark",
-            start,
-            end,
-            markType,
-            attrs: { url },
-        },
+            {
+                path: ["text"],
+                action: "addMark",
+                start,
+                end,
+                markType,
+                attrs: { url },
+            },
         ])
         return change
     }
     else if (markType === "comment") {
-        // const id = "comment-" + crypto.randomBytes(4).toString('hex')
+        const id = "comment-" + crypto.randomBytes(4).toString('hex')
+        commentHistory.push(id)
         const { change } = doc.change([
-        {
-            path: ["text"],
-            action: "addMark",
-            start,
-            end,
-            markType,
-            
-            attrs: { id: "abc-123" }, 
-            //attrs: { id },
-        },
+            {
+                path: ["text"],
+                action: "addMark",
+                start,
+                end,
+                markType,
+                attrs: { id },
+            },
         ])
         return change
     }
     else {
         const { change } = doc.change([
-        {
-            path: ["text"],
-            action: "addMark",
-            start,
-            end,
-            markType
-        },
+            {
+                path: ["text"],
+                action: "addMark",
+                start,
+                end,
+                markType
+            },
         ])
         return change
     }
@@ -66,44 +67,42 @@ function removeMarkChange(doc: Micromerge) {
     const end = start + Math.floor(Math.random() * (length - start - 1))
     const markType = markTypes[Math.floor(Math.random() * markTypes.length)];
     if (markType === "link") {
-        const url = exampleURLs[Math.floor(Math.random() * exampleURLs.length)];
         const { change } = doc.change([
-        {
-            path: ["text"],
-            action: "removeMark",
-            start,
-            end,
-            markType,
-            attrs: { url },
-        },
+            {
+                path: ["text"],
+                action: "removeMark",
+                start,
+                end,
+                markType,
+            },
         ])
         return change
     }
     else if (markType === "comment") {
-        // const id = "comment-" + crypto.randomBytes(4).toString('hex')
+        // note to gklitt: we should probably enumerate the existing comments, right now it just grows
+        const id = commentHistory[Math.floor(Math.random() * commentHistory.length)];
+
         const { change } = doc.change([
-        {
-            path: ["text"],
-            action: "removeMark",
-            start,
-            end,
-            markType,
-            
-            attrs: { id: "abc-123" }, 
-            //attrs: { id },
-        },
+            {
+                path: ["text"],
+                action: "removeMark",
+                start,
+                end,
+                markType,
+                attrs: { id },
+            },
         ])
         return change
     }
     else {
         const { change } = doc.change([
-        {
-            path: ["text"],
-            action: "removeMark",
-            start,
-            end,
-            markType
-        },
+            {
+                path: ["text"],
+                action: "removeMark",
+                start,
+                end,
+                markType
+            },
         ])
         return change
     }
@@ -148,21 +147,21 @@ function removeChange(doc: Micromerge) {
     return change
 }
 
-const {doc1, doc2} = generateDocs("alphabet")
+const { doc1, doc2 } = generateDocs("alphabet")
 const doc1Queue: Change[] = []
 const doc2Queue: Change[] = []
 const opTypes = ["insert", "remove", "addMark", "removeMark"]
 
 // eslint-disable-next-line no-constant-condition
 let totalChanges = 0
-while(totalChanges++ < 1_000_000) {
+while (totalChanges++ < 1_000_000) {
     if (totalChanges % 1000 == 0) { console.log("Total changes: ", totalChanges) }
     const randomTarget = (Math.random() < 0.5)
     const doc = randomTarget ? doc1 : doc2
     const queue = randomTarget ? doc1Queue : doc2Queue
 
     const op = opTypes[Math.floor(Math.random() * opTypes.length)];
-    switch(op) {
+    switch (op) {
         case "insert":
             // console.log(`I ${randomTarget ? "doc1" : "doc2"}`)
             queue.push(insertChange(doc))
@@ -179,7 +178,7 @@ while(totalChanges++ < 1_000_000) {
             // console.log(`R ${randomTarget ? "doc1" : "doc2"}`)
             queue.push(removeMarkChange(doc))
             break
-        }
+    }
 
     const shouldSync = (Math.random() < 0.1)
     if (shouldSync) {

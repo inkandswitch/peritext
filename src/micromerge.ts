1a0c0ad2c3ed5c908c7123f8a53971c36ea8f972
Geoffrey Litt <gklitt@gmail.com>
2021-11-09 22:44:58 -0500

    reformat code

--- src/micromerge.ts
+++ src/micromerge.ts
@@ -27,8 +27,8 @@ type CONTENT_KEY = "text"
 
 export type MarkMapWithoutOpIds = {
     [K in MarkType]?: Marks[K]["allowMultiple"] extends true
-    ? Array<WithoutOpId<MarkValue[K]>>
-    : WithoutOpId<MarkValue[K]>
+        ? Array<WithoutOpId<MarkValue[K]>>
+        : WithoutOpId<MarkValue[K]>
 }
 
 type WithoutOpId<M extends Values<MarkValue>> = Omit<M, "opId">
@@ -176,19 +176,19 @@ interface RemoveMarkOperationInputBase<M extends MarkType> {
 
 export type RemoveMarkOperationInput =
     | (RemoveMarkOperationInputBase<"strong"> & {
-        attrs?: undefined
-    })
+          attrs?: undefined
+      })
     | (RemoveMarkOperationInputBase<"em"> & {
-        attrs?: undefined
-    })
+          attrs?: undefined
+      })
     | (RemoveMarkOperationInputBase<"comment"> & {
-        /** Data attributes for the mark. */
-        attrs: Omit<MarkValue["comment"], "opId">
-    })
+          /** Data attributes for the mark. */
+          attrs: Omit<MarkValue["comment"], "opId">
+      })
     | (RemoveMarkOperationInputBase<"link"> & {
-        /** Data attributes for the mark. */
-        attrs?: undefined
-    })
+          /** Data attributes for the mark. */
+          attrs?: undefined
+      })
 
 export type InputOperation =
     | MakeListOperationInput
@@ -305,9 +305,9 @@ type RemoveMarkOperation =
     | RemoveMarkOperationBase<"strong">
     | RemoveMarkOperationBase<"em">
     | (RemoveMarkOperationBase<"comment"> & {
-        /** Data attributes for the mark. */
-        attrs: DistributiveOmit<MarkValue["comment"], "opId">
-    })
+          /** Data attributes for the mark. */
+          attrs: DistributiveOmit<MarkValue["comment"], "opId">
+      })
     | RemoveMarkOperationBase<"link">
 
 export type Operation =
@@ -366,14 +366,14 @@ type Metadata = ListMetadata | MapMetadata<Record<string, Json>>
 
 type BooleanMarkValue =
     | {
-        active: true
-        /** A MarkValue should always have the ID of the operation that last modified it. */
-        opId: OperationId
-    }
+          active: true
+          /** A MarkValue should always have the ID of the operation that last modified it. */
+          opId: OperationId
+      }
     | {
-        active: false
-        opId: OperationId
-    }
+          active: false
+          opId: OperationId
+      }
 
 type IdMarkValue = {
     id: string
@@ -383,16 +383,16 @@ type IdMarkValue = {
 
 type LinkMarkValue =
     | {
-        url: string
-        /** A MarkValue should always have the ID of the operation that last modified it. */
-        opId: OperationId
-        active: true
-    }
+          url: string
+          /** A MarkValue should always have the ID of the operation that last modified it. */
+          opId: OperationId
+          active: true
+      }
     | {
-        url?: undefined
-        opId: OperationId
-        active: false
-    }
+          url?: undefined
+          opId: OperationId
+          active: false
+      }
 
 export type MarkValue = Assert<
     {
